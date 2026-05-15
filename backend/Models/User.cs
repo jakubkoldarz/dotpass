1d789b3c55809bd183c661507a65b44c31c7b3bb
Jakub Kołdarz <jakub213x@gmail.com>
2026-05-15 11:27:21 +0200

    Added User model

---
+++ backend/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(255)]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(8)]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string Firstname { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string Lastname { get; set; } = string.Empty;
+
+    }
+}
