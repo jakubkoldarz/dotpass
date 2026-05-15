5676b21eb01bcda90d35829abb32286822a5bdc2
Jakub Kołdarz <jakub213x@gmail.com>
2026-05-15 11:35:18 +0200

    Added Workspace and Device models

---
+++ backend/Models/Workspace.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class Workspace
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
